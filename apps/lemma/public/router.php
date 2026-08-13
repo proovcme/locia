deb1e9efe61085164e4bd0d0f7fe6841100a0c0a
Locia Public <hello@locia.work>
2026-08-13 21:22:32 +0300

    Normalize Lemma reverse-proxy base path

--- apps/lemma/public/router.php
+++ apps/lemma/public/router.php
@@ -2,7 +2,16 @@
 
 declare(strict_types=1);
 
-$path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
+$requestUri = (string) ($_SERVER['REQUEST_URI'] ?? '/');
+$path = parse_url($requestUri, PHP_URL_PATH) ?: '/';
+$basePath = rtrim((string) (parse_url((string) (getenv('APP_URL') ?: ''), PHP_URL_PATH) ?: ''), '/');
+$strippedBasePath = false;
+if ($basePath !== '' && ($path === $basePath || str_starts_with($path, $basePath . '/'))) {
+    $path = substr($path, strlen($basePath)) ?: '/';
+    $query = parse_url($requestUri, PHP_URL_QUERY);
+    $_SERVER['REQUEST_URI'] = $path . ($query !== null && $query !== '' ? '?' . $query : '');
+    $strippedBasePath = true;
+}
 $file = __DIR__ . $path;
 $isDemoMode = (string) (getenv('DEMO_MODE') ?: '') === '1';
 
@@ -19,6 +28,20 @@ if ($isDemoMode && preg_match('#^/locia-atlas(?:/|$)#', $path)) {
 }
 
 if ($path !== '/' && (is_file($file) || (is_dir($file) && is_file(rtrim($file, '/') . '/index.html')))) {
+    if ($strippedBasePath && is_file($file)) {
+        $types = [
+            'css' => 'text/css; charset=utf-8',
+            'js' => 'application/javascript; charset=utf-8',
+            'json' => 'application/json; charset=utf-8',
+            'webmanifest' => 'application/manifest+json; charset=utf-8',
+            'svg' => 'image/svg+xml',
+            'png' => 'image/png',
+        ];
+        $extension = strtolower((string) pathinfo($file, PATHINFO_EXTENSION));
+        header('Content-Type: ' . ($types[$extension] ?? 'application/octet-stream'));
+        readfile($file);
+        exit;
+    }
     return false;
 }
 
